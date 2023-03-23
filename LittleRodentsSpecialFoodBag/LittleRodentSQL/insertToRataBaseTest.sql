748d56e567c0bcd26ea9d22fff672fa09e8007d9
NikKats <72512506+NikKats@users.noreply.github.com>
2023-03-23 14:31:32 +0200

    Add files via upload

---
+++ LittleRodentsSpecialFoodBag/LittleRodentSQL/insertToRataBaseTest.sql
@@ -0,0 +1,13 @@
+USE littleRodent;
+
+INSERT INTO foodPlaces(name, category)
+VALUES("Bairaktaris","Traditional"),
+("Ladokolla","Traditional"),
+("Vaggos","Grill"),
+("Grill Brothers","Grill"),
+("Leo d'Oro","Italian"),
+("Roma Pizza","Italian"),
+("Jiang Nan","Asian"),
+("Noodle Bar","Asian"),
+("El burrito","Mexican"),
+("Tres Caballeros","Mexican");
