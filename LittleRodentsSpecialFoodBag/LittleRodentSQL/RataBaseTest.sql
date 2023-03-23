d03354ca1ff2ee4f4031968706de86c5693e36cf
NikKats <72512506+NikKats@users.noreply.github.com>
2023-03-23 14:29:33 +0200

    Add files via upload

---
+++ LittleRodentsSpecialFoodBag/LittleRodentSQL/RataBaseTest.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS littleRodentTest;
+CREATE DATABASE littleRodentTest;
+
+USE littleRodentTest;
+
+CREATE TABLE foodPlaces(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+	category VARCHAR(20) NOT NULL,
+	deliveryTimeScore FLOAT,
+	priceScore FLOAT,
+	tastinessScore FLOAT, 
+	overallScore FLOAT
+);
+
+CREATE TABLE orders(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    foodPlaceId INT NOT NULL,
+    orderStatus VARCHAR(20) NOT NULL,
+    foodBundle VARCHAR(30) NOT NULL,
+    FOREIGN KEY (foodPlaceId) REFERENCES foodPlaces(id) ON DELETE CASCADE
+    );
+    
+CREATE TABLE reviews(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    orderId INT NOT NULL,
+	deliverytimeScore INT NOT NULL,
+	priceScore INT NOT NULL,
+	tastinessScore INT NOT NULL,
+    bundleScore INT NOT NULL,
+    FOREIGN KEY (orderId) REFERENCES orders(id) ON DELETE CASCADE
+    );
